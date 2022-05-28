c64bb797f2949a02ce856cfac312ba0ab97208c2
Adriel <adriel.fsantos@outlook.com>
2022-05-27 21:36:33 -0300

    Adiciona view para buscar sala por nome

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import ListCreateAPIView, ListAPIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from salas.models import Sala
 from salas.serializers import SalaSerializer
 
@@ -11,3 +13,10 @@ class ListarSalas(ListCreateAPIView):
 class SalasDisponiveis(ListAPIView):
     queryset = Sala.objects.filter(disponivel=True)
     serializer_class = SalaSerializer
+
+
+@api_view(['GET'])
+def buscar_salas(request, nome):
+    salas = Sala.objects.filter(nome__icontains=nome)
+    serializer = SalaSerializer(salas, many=True)
+    return Response(serializer.data)

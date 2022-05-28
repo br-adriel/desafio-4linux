494a8b7c2b55762bb59963f447a68639f52d5d4b
Adriel <adriel.fsantos@outlook.com>
2022-05-27 21:35:28 -0300

    Adiciona rota para listar salas disponiveis

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ListarSalas
+from .views import ListarSalas, SalasDisponiveis
 
 
 urlpatterns = [
   path('salas', ListarSalas.as_view(), name='salas'),
+  path('salas/disponivel', SalasDisponiveis.as_view(), name='salas-disponiveis'),
 ]

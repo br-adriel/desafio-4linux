3343bb15539afbd4048ebe24c787b7a21663bdfe
Adriel <adriel.fariasan@gmail.com>
2022-05-28 12:49:52 -0300

    Adiciona rota para agendamento de sala

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ListarSalas, SalasDisponiveis, buscar_salas
+from .views import AgendarSala, ListarSalas, SalasDisponiveis, buscar_salas
 
 
 urlpatterns = [
   path('salas', ListarSalas.as_view(), name='salas'),
   path('salas/disponivel', SalasDisponiveis.as_view(), name='salas-disponiveis'),
   path('salas/buscar/<str:nome>', buscar_salas, name='buscar-salas'),
+  path('salas/agendar', AgendarSala.as_view(), name='agendar-sala')
 ]

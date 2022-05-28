ad6fb9dbe7b95cd494d57063a1f548aaa8c3f17e
Adriel <adriel.fsantos@outlook.com>
2022-05-27 21:36:53 -0300

    Adiciona rota para busca de sala por nome

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ListarSalas, SalasDisponiveis
+from .views import ListarSalas, SalasDisponiveis, buscar_salas
 
 
 urlpatterns = [
   path('salas', ListarSalas.as_view(), name='salas'),
   path('salas/disponivel', SalasDisponiveis.as_view(), name='salas-disponiveis'),
+  path('salas/buscar/<str:nome>', buscar_salas, name='buscar-salas'),
 ]

f52ad90be755d0c05e20761bf2c4f9684167a17a
Adriel <adriel.fsantos@outlook.com>
2022-05-27 21:35:13 -0300

    Adiciona views para listar salas disponiveis

--- api/views.py
+++ api/views.py
@@ -1,8 +1,13 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, ListAPIView
 from salas.models import Sala
 from salas.serializers import SalaSerializer
 
 
 class ListarSalas(ListCreateAPIView):
     queryset = Sala.objects.all()
+    serializer_class = SalaSerializer
+
+
+class SalasDisponiveis(ListAPIView):
+    queryset = Sala.objects.filter(disponivel=True)
     serializer_class = SalaSerializer
